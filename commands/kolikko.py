94b8bcf4aa7a106080d107e214145269f5b0f8af
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2024-04-24 05:17:16 +0300

    Delete commands directory
    deleted old text commands.
    These files will be replaced with slash commands.

--- commands/kolikko.py
+++
@@ -1,8 +0,0 @@
-# commands/kolikko.py
-import random
-
-async def execute(message, args, client, commands):
-    result = random.choice(['Kruuna', 'Klaava'])
-    await message.channel.send(f"Kolikon heiton tulos: {result}")
-
-execute.__command_name__ = 'kolikko'
--- commands/ping.py
+++
@@ -1,8 +0,0 @@
-# commands/ping.py
-
-async def execute(message, args, client, commands):
-    latency = round(client.latency * 1000)  # latency is in seconds, convert to milliseconds
-    await message.channel.send(f'Pong! Latency: {latency}ms')
-
-# Assign the execute function to a 'name' attribute
-execute.__command_name__ = 'ping'
