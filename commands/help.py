a52b0e92e23c195dbdcb8c1f0578f4dcdf1e4fae
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2024-05-08 16:19:39 +0300

    Update help.py
    added mute and unmute commands to help command

--- commands/help.py
+++ commands/help.py
@@ -7,5 +7,7 @@ def setup(client):
         embed = discord.Embed(title="Bot Commands")
         embed.add_field(name="/help", value="Shows all avaible commands", inline=False)
         embed.add_field(name="/poll", value="Creates poll", inline=False)
+        embed.add_field(name="/mute", value="Mutes user with role muted (reguires manage role permissions)", inline=False)
+        embed.add_field(name="/unmute", value="Unmutes user with removing muted role (requires manage roles permission)", inline=False)
         embed.add_field(name="/clear", value="clears defined amount of messages", inline=False)
         await interaction.response.send_message(embed=embed)
