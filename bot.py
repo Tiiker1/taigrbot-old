86cb84d616753a3a8c5b00fa852bd1fe06b0fcaa
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2024-05-08 16:29:27 +0300

    Update bot.py
    fixed imports for github scripts which now are in  scripts folder

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 from discord import app_commands
 import pytz
 import asyncio
-from github import fetch_recent_commits, load_commit_data, save_commit_data, create_commit_embed
-from github import check_commits_and_send_message
+from scripts.github import fetch_recent_commits, load_commit_data, save_commit_data, create_commit_embed
+from scripts.github import check_commits_and_send_message
 
 from commands import bug_contextmenu
 from commands import report_contextmenu
