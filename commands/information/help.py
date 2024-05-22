e723e9f8d872bdc6a9e56d4606a521f092b3704e
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2024-05-22 14:19:37 +0300

    Update help.py
    added following commands to help command:
    /review
    /visitturku

--- commands/information/help.py
+++ commands/information/help.py
@@ -7,4 +7,6 @@ def setup(client):
         embed = discord.Embed(title="Bot Commands")
         embed.add_field(name="/help", value="Shows all avaible commands", inline=False)
         embed.add_field(name="/poll", value="Creates poll", inline=False)
+        embed.add_field(name="/review", value="creates product review with assisted fields", inline=False)
+        embed.add_field(name="/visitturku", value="sends events from visitturku.fi", inline=False)
         await interaction.response.send_message(embed=embed)
