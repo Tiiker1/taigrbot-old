ccc6fc192acf37ce236bedcc079fc0e72e4c6d94
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2024-07-03 15:00:40 +0300

    Update bot.py
    Updated bot to use global command registeration so it register commands to all servers its on instead of only to hardcoded guild

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
-from typing import Optional
 import discord
 from discord.ext import commands
 from discord import app_commands
-import pytz
 import asyncio
 
 from scripts.github import fetch_all_commits, load_commit_data, save_commit_data, create_commit_embed
@@ -20,13 +18,11 @@ from commands.information import help
 from commands.information import ahelp
 from commands.information import permissions
 
-from commands.xpsystem import xp
-from commands.xpsystem import leaderboard
-
 from commands.features.poll import setup as setup_poll
 from commands.features import review
 
-MY_GUILD = discord.Object(id=guildidhere)  # replace with your guild id
+from commands.xpsystem import xp
+from commands.xpsystem import leaderboard
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -35,36 +31,41 @@ class MyClient(discord.Client):
         self.xp_database = XPDatabase()
 
     async def setup_hook(self):
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+        await self.tree.sync()  # Syncs commands globally
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 client = MyClient(intents=intents)
 
 # Call setup functions
 report_contextmenu.setup(client)
 bug_contextmenu.setup(client)
+
 help.setup(client)
 ahelp.setup(client)
 permissions.setup(client)
+
 mute.setup(client)
 unmute.setup(client)
 rolemenu.setup(client)
 setup_clear(client)
+
 setup_poll(client)
+review.setup(client)
+
 xp.setup(client)
 leaderboard.setup(client)
 
-# Dictionary to store the channel IDs for each guild
-welcome_leave_channels = {
-    23423424242423424: 2342424234324,  # Guild ID : Channel ID -> remeber to place with actual guild and channel id´s 
-}
-
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    print('client is ready.')
+    await client.change_presence(activity=discord.CustomActivity(name='whateveryouwanttoplay', emoji='🖥️'))
     await check_commits_and_send_message(client)
     asyncio.create_task(schedule_commit_check(client))
+
+# Dictionary to store the channel IDs for each guild
+welcome_leave_channels = {
+    putyourguildidhere: andtextchannelforwelcomeandleavemessageshere,  # Guild ID : Channel ID
+}
 
 @client.event
 async def on_member_join(member):
@@ -94,10 +95,10 @@ async def on_message(message):
         new_level = client.xp_database.add_xp(guild_id, message.author.id, 10)  # Award 10 XP for each message
         if new_level:
             await message.channel.send(f"Congratulations {message.author.mention}, you've leveled up to level {new_level}!")
-        
+
 async def schedule_commit_check(client):
     while True:
-        await asyncio.sleep(600)  # Sleep for 1 hour (3600 seconds)
+        await asyncio.sleep(600)  # Sleep for 10 minutes (600 seconds)
         await check_commits_and_send_message(client)
 
 client.run("put your bot token here")
